5459629676046cf7e51f2508cd49d8570cee6436
Ravi9038 <ravijadhav9038@gmail.com>
2020-07-15 13:34:12 +0530

    Take value from USer

--- Day10Prob01Airthmatic.sh
+++ Day10Prob01Airthmatic.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 
 echo "Airthmatic Operation"
+
+#Take input three fromm User UseCase 1
+
+echo "Enter the Three Inputs from user"
+read -p "Enter First Value : " firstNum
+read -p "Enter the Second Number : " secNum
+read -p "Enter the Third Number : " thirdNum
+
+echo "First Number = "$firstNum
+echo "Second Number = "$secNum
+echo "Third Number = "$thirdNum
