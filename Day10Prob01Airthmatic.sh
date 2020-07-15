3d29ea10160b57d061164447b11079becceae4d6
ravi9038 <ravijadhav9038@gmail.com>
2020-07-15 16:20:16 +0530

    Decending Order

--- Day10Prob01Airthmatic.sh
+++ Day10Prob01Airthmatic.sh
@@ -30,8 +30,23 @@ echo "Operations storing into the dictionary : ${addDict[@]}"
 count=0
 for i in "${addDict[@]}"
 do
-#storing the dictionary values into the array
-arr[((count++))]=$i
+	#storing the dictionary values into the array
+	arr[((count++))]=$i
 done
 #Print the array
 echo "Array is ${arr[@]}"
+
+#sorting the computational values decending order that result shows
+for((i=0;i<4;i++))
+do
+	for((j = 0; j<4-i-1; j++))
+	do
+		if [ ${arr[j]} -lt ${arr[$((j+1))]} ]
+		then
+			temp=${arr[j]}
+			arr[$j]=${arr[$((j+1))]}
+			arr[$((j+1))]=$temp
+		fi
+	done
+done
+echo "Sorted Array decending order ${arr[*]}"
