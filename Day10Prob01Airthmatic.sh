d257e511e5f07d50fad00e9ccd2866a3294b2ea5
ravi9038 <ravijadhav9038@gmail.com>
2020-07-15 16:22:43 +0530

    Asecnding Order

--- Day10Prob01Airthmatic.sh
+++ Day10Prob01Airthmatic.sh
@@ -50,3 +50,18 @@ do
 	done
 done
 echo "Sorted Array decending order ${arr[*]}"
+
+#sorting the computational values ascending order that result shows
+for((i=0;i<4;i++))
+do
+	for((j = 0; j<4-i-1; j++))
+	do
+		if [ ${arr[j]} -gt ${arr[$((j+1))]} ]
+		then
+			temp=${arr[j]}
+			arr[$j]=${arr[$((j+1))]}
+			arr[$((j+1))]=$temp
+		fi
+	done
+done
+echo "Sorted Array ascending order ${arr[*]}"
